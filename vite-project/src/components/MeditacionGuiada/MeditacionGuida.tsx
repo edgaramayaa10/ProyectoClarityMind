8b8595f594d98133220c5d91d96ab3c63bd6af0f
Edgar Amaya <160590740+edgaramayaa10@users.noreply.github.com>
2024-07-12 13:17:05 +0200

    color botones

--- vite-project/src/components/MeditacionGuiada/MeditacionGuida.tsx
+++ vite-project/src/components/MeditacionGuiada/MeditacionGuida.tsx
@@ -14,28 +14,28 @@ const MeditacionGuiada = () => {
                 <Typography sx={{ color: 'black', textAlign: 'center', fontFamily: 'monospace', fontSize: '20px', marginBottom: '20px' }} variant='h6'>Es una herramienta que permite, mediante palabras e imágenes, dejar de lado emociones negativas como el dolor, el estrés y las preocupaciones del día a día, ayudándonos a conectar con nuestro interior.</Typography>
                 <Grid container spacing={2} justifyContent="center" marginTop={5}>
                     <Grid item xs={6} sm={3}>
-                        <Button variant="outlined" fullWidth sx={{ height: '100%', width: '100%', color:'#045346', borderColor:'#045346' }} className="custom-button">Meditaciones tradicionales</Button>
+                        <Button variant="outlined" fullWidth sx={{ height: '100%', width: '100%', color:'#045346','&:hover':{borderColor:'#1A4D2E'}, borderColor:'#045346' }} className="custom-button">Meditaciones tradicionales</Button>
                     </Grid>
                     <Grid item xs={6} sm={3}>
-                        <Button variant="outlined" fullWidth sx={{ height: '100%', width: '100%', color:'#045346', borderColor:'#045346' }} className="custom-button">Meditación con visualizaciones</Button>
+                        <Button variant="outlined" fullWidth sx={{ height: '100%', width: '100%', color:'#045346','&:hover':{borderColor:'#1A4D2E'}, borderColor:'#045346' }} className="custom-button">Meditación con visualizaciones</Button>
                     </Grid>
                     <Grid item xs={6} sm={3}>
-                        <Button variant="outlined" fullWidth sx={{ height: '100%', width: '100%', color:'#045346', borderColor:'#045346' }} className="custom-button">Relajación y escaneo corporal</Button>
+                        <Button variant="outlined" fullWidth sx={{ height: '100%', width: '100%', color:'#045346','&:hover':{borderColor:'#1A4D2E'}, borderColor:'#045346' }} className="custom-button">Relajación y escaneo corporal</Button>
                     </Grid>
                     <Grid item xs={6} sm={3}>
-                        <Button variant="outlined" fullWidth sx={{ height: '100%', width: '100%', color:'#045346', borderColor:'#045346' }} className="custom-button">Tonos binaurales</Button>
+                        <Button variant="outlined" fullWidth sx={{ height: '100%', width: '100%', color:'#045346','&:hover':{borderColor:'#1A4D2E'}, borderColor:'#045346' }} className="custom-button">Tonos binaurales</Button>
                     </Grid>
                     <Grid item xs={6} sm={3}>
-                        <Button variant="outlined" fullWidth sx={{ height: '100%', width: '100%', color:'#045346', borderColor:'#045346' }} className="custom-button">Afirmaciones</Button>
+                        <Button variant="outlined" fullWidth sx={{ height: '100%', width: '100%', color:'#045346','&:hover':{borderColor:'#1A4D2E'}, borderColor:'#045346' }} className="custom-button">Afirmaciones</Button>
                     </Grid>
                     <Grid item xs={6} sm={3}>
-                        <Button variant="outlined" fullWidth sx={{ height: '100%', width: '100%', color:'#045346', borderColor:'#045346' }} className="custom-button">Meditación guiada de atención a la respiración</Button>
+                        <Button variant="outlined" fullWidth sx={{ height: '100%', width: '100%', color:'#045346','&:hover':{borderColor:'#1A4D2E'}, borderColor:'#045346' }} className="custom-button">Meditación guiada de atención a la respiración</Button>
                     </Grid>
                     <Grid item xs={6} sm={3}>
-                        <Button variant="outlined" fullWidth sx={{ height: '100%', width: '100%', color:'#045346', borderColor:'#045346' }} className="custom-button">Mindfulness</Button>
+                        <Button variant="outlined" fullWidth sx={{ height: '100%', width: '100%', color:'#045346','&:hover':{borderColor:'#1A4D2E'}, borderColor:'#045346' }} className="custom-button">Mindfulness</Button>
                     </Grid>
                     <Grid item xs={6} sm={3}>
-                        <Button variant="outlined" fullWidth sx={{ height: '100%', width: '100%', color:'#045346', borderColor:'#045346' }} className="custom-button">Meditaciones guiadas para dormir mejor</Button>
+                        <Button variant="outlined" fullWidth sx={{ height: '100%', width: '100%', color:'#045346','&:hover':{borderColor:'#1A4D2E'}, borderColor:'#045346' }} className="custom-button">Meditaciones guiadas para dormir mejor</Button>
                     </Grid>
                 </Grid>
                 </Box>
