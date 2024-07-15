d406cf497ef4fd4f76361acfaf4756550782f272
Edgar Amaya <160590740+edgaramayaa10@users.noreply.github.com>
2024-07-15 10:54:12 +0200

    meditacion guiada barra

--- vite-project/src/components/MeditacionGuiada/MeditacionGuida.tsx
+++ vite-project/src/components/MeditacionGuiada/MeditacionGuida.tsx
@@ -1,49 +1,83 @@
-import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
-import { Link } from 'react-router-dom';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';  
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
 
 const MeditacionGuiada = () => {
     return (
-        <React.Fragment>
+        <Box sx={{ display: 'flex', }}>
             <CssBaseline />
-            <Box sx={{ bgcolor: 'white', color: '#045346', height: '370px', p: 2 }}>
-            <Typography sx={{ fontWeight: 'bold', textAlign: 'center', fontFamily: 'monospace', fontSize: '35px', margin: '20px 0' }} variant='h5'>Meditación Guiada</Typography>
-                <Typography sx={{ color: 'black', textAlign: 'center', fontFamily: 'monospace', fontSize: '20px', marginBottom: '20px' }} variant='h6'>Es una herramienta que permite, mediante palabras e imágenes, dejar de lado emociones negativas como el dolor, el estrés y las preocupaciones del día a día, ayudándonos a conectar con nuestro interior.</Typography>
-                <Grid container spacing={2} justifyContent="center" marginTop={5}>
-                    <Grid item xs={6} sm={3}>
-                    <Link to='/meditacion/meditacionTradicional'><Button variant="outlined" fullWidth sx={{ height: '100%', width: '100%', color:'#045346','&:hover':{borderColor:'#1A4D2E'}, borderColor:'#045346' }} className="custom-button">Meditaciones tradicionales</Button></Link> 
-                    </Grid>
-                    <Grid item xs={6} sm={3}>
-                    <Link to='/meditacion/meditacionVisualizaciones'><Button variant="outlined" fullWidth sx={{ height: '100%', width: '100%', color:'#045346','&:hover':{borderColor:'#1A4D2E'}, borderColor:'#045346' }} className="custom-button">Meditación con visualizaciones</Button></Link> 
-                    </Grid>
-                    <Grid item xs={6} sm={3}>
-                    <Link to='/meditacion/meditacionRelajacion'><Button variant="outlined" fullWidth sx={{ height: '100%', width: '100%', color:'#045346','&:hover':{borderColor:'#1A4D2E'}, borderColor:'#045346' }} className="custom-button">Relajación y escaneo corporal</Button></Link> 
-                    </Grid>
-                    <Grid item xs={6} sm={3}>
-                    <Link to='/meditacion/meditacionBinaurales'><Button variant="outlined" fullWidth sx={{ height: '100%', width: '100%', color:'#045346','&:hover':{borderColor:'#1A4D2E'}, borderColor:'#045346' }} className="custom-button">Tonos binaurales</Button></Link> 
-                    </Grid>
-                    <Grid item xs={6} sm={3}>
-                    <Link to='/meditacion/meditacionAfirmaciones'><Button variant="outlined" fullWidth sx={{ height: '100%', width: '100%', color:'#045346','&:hover':{borderColor:'#1A4D2E'}, borderColor:'#045346' }} className="custom-button">Afirmaciones</Button></Link> 
-                    </Grid>
-                    <Grid item xs={6} sm={3}>
-                    <Link to='/meditacion/meditacionRespiracion'><Button variant="outlined" fullWidth sx={{ height: '100%', width: '100%', color:'#045346','&:hover':{borderColor:'#1A4D2E'}, borderColor:'#045346' }} className="custom-button">Meditación guiada de atención a la respiración</Button></Link> 
-                    </Grid>
-                    <Grid item xs={6} sm={3}>
-                    <Link to='/meditacion/meditacionMindfunless'><Button variant="outlined" fullWidth sx={{ height: '100%', width: '100%', color:'#045346','&:hover':{borderColor:'#1A4D2E'}, borderColor:'#045346' }} className="custom-button">Mindfulness</Button></Link> 
-                    </Grid>
-                    <Grid item xs={6} sm={3}>
-                    <Link to='/meditacion/meditacionDormir'><Button variant="outlined" fullWidth sx={{ height: '100%', width: '100%', color:'#045346','&:hover':{borderColor:'#1A4D2E'}, borderColor:'#045346' }} className="custom-button">Meditaciones guiadas para dormir mejor</Button></Link> 
-                    </Grid>
-                </Grid>
-                </Box>
+            <AppBar
+                position="fixed"
+                sx={{ width: `calc(50% - ${20}px)`, ml: `${20}px`, marginTop:'100px' }}
+            >
+            </AppBar>
+            <Drawer
+                sx={{
+                    width: 20,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: 300,
+                        boxSizing: 'border-box',
+                        marginTop: '100px'
+                    },
+                }}
+                variant="permanent"
+                anchor="left"
+            >
+                <List sx={{}}>
+                    {['Meditaciones Tradicionales', 'Meditacion con visualizaciones', 'Relajacion y escaneo corporal', 'Tonos Binaurales', 'Afirmaciones', 'Meditacion guiada de atencion a la respiracion', 'Mindfunless', 'Meditacion para dormir mejor'].map((text) => (
+                        <ListItem key={text} disablePadding>
+                            <ListItemButton>
+                                <ListItemText primary={text} />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
 
-        </React.Fragment>
-
+            </Drawer>
+            <Box
+                component="main"
+                sx={{ flexGrow: 5, bgcolor: 'background.default', p: 3, marginLeft:'280px' }}
+            >
+                <Toolbar />
+                <Typography paragraph>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+                    enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+                    imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
+                    Convallis convallis tellus id interdum velit laoreet id donec ultrices.
+                    Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
+                    nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
+                    leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
+                    feugiat vivamus at augue. At augue eget arcu dictum varius duis at
+                    consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
+                    sapien faucibus et molestie ac.
+                </Typography>
+                <Typography paragraph>
+                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
+                    eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
+                    neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
+                    tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
+                    sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
+                    tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
+                    gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
+                    et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
+                    tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+                    eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+                    posuere sollicitudin aliquam ultrices sagittis orci a.
+                </Typography>
+            </Box>
+        </Box>
     );
 }
-export default MeditacionGuiada;
 
+export default MeditacionGuiada;
