b60ba82648d6617f09d6cd22b84ad00673a6e5aa
GPA9 <guillempereira@hotmail.com>
2024-07-15 13:41:04 +0200

    Refresh

--- vite-project/src/components/MeditacionGuiada/MeditacionGuida.tsx
+++ vite-project/src/components/MeditacionGuiada/MeditacionGuida.tsx
@@ -1,27 +1,23 @@
-<<<<<<< HEAD
-import { useState } from 'react';
-=======
-import React, { useState } from 'react';
->>>>>>> 4678929b3cadde873c6d75a0e8fbe02ec80a0bff
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import AppBar from "@mui/material/AppBar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
 
 // Importa los componentes de contenido específicos
-import MeditacionesTradicionales from './MeditacionesTradicionales';
-import MeditacionVisualizaciones from './MeditacionVisualizaciones';
-import RelajacionEscaneo from './RelajacionEscaneo';
-import TonosBinaturales from './TonosBinaturales';
-import Afirmaciones from './Afirmaciones';
-import MeditacionRespiracion from './MeditacionRespiracion';
-import Mindfunless from './Mindfunless';
-import MeditacionDormir from './MeditacionDormir';
+import MeditacionesTradicionales from "./MeditacionesTradicionales";
+import MeditacionVisualizaciones from "./MeditacionVisualizaciones";
+import RelajacionEscaneo from "./RelajacionEscaneo";
+import TonosBinaturales from "./TonosBinaturales";
+import Afirmaciones from "./Afirmaciones";
+import MeditacionRespiracion from "./MeditacionRespiracion";
+import Mindfunless from "./Mindfunless";
+import MeditacionDormir from "./MeditacionDormir";
 
 const MeditacionGuiada = () => {
   const [selectedText, setSelectedText] = useState<string | null>(null);
@@ -32,45 +28,51 @@ const MeditacionGuiada = () => {
 
   const renderSelectedContent = () => {
     switch (selectedText) {
-      case 'Meditaciones Tradicionales':
+      case "Meditaciones Tradicionales":
         return <MeditacionesTradicionales />;
-      case 'Meditacion con visualizaciones':
+      case "Meditacion con visualizaciones":
         return <MeditacionVisualizaciones />;
-      case 'Relajacion y escaneo corporal':
+      case "Relajacion y escaneo corporal":
         return <RelajacionEscaneo />;
-      case 'Tonos Binaturales':
-        return <TonosBinaturales/>;
-      case 'Afirmaciones':
+      case "Tonos Binaturales":
+        return <TonosBinaturales />;
+      case "Afirmaciones":
         return <Afirmaciones />;
-      case 'Meditacion guiada de atencion a la respiracion':
+      case "Meditacion guiada de atencion a la respiracion":
         return <MeditacionRespiracion />;
-      case 'Mindfunless':
+      case "Mindfunless":
         return <Mindfunless />;
-      case 'Meditacion para dormir mejor':
+      case "Meditacion para dormir mejor":
         return <MeditacionDormir />;
-        
+
       // Agrega más casos para cada elemento de la lista
       default:
         return (
-          <Typography paragraph>
-            Seleccione un elemento de la lista para ver más información.
-          </Typography>
+          <div>
+            <Typography variant="h5">Meditacion</Typography>
+            <Typography paragraph>
+              Aquí va el texto específico para Meditacion.
+            </Typography>
+          </div>
         );
     }
   };
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <AppBar position="fixed" sx={{ width: `calc(50% - ${20}px)`, ml: `${20}px` }}></AppBar>
+      <AppBar
+        position="fixed"
+        sx={{ width: `calc(50% - ${20}px)`, ml: `${20}px` }}
+      ></AppBar>
       <Drawer
         sx={{
           width: 20,
           flexShrink: 0,
-          '& .MuiDrawer-paper': {
+          "& .MuiDrawer-paper": {
             width: 300,
-            boxSizing: 'border-box',
-            marginTop: '100px',
+            boxSizing: "border-box",
+            marginTop: "100px",
           },
         }}
         variant="permanent"
@@ -78,25 +80,43 @@ const MeditacionGuiada = () => {
       >
         <List sx={{ paddingTop: 0, paddingBottom: 0 }}>
           {[
-            'Meditaciones Tradicionales',
-            'Meditacion con visualizaciones',
-            'Relajacion y escaneo corporal',
-            'Tonos Binaturales',
-            'Afirmaciones',
-            'Meditacion guiada de atencion a la respiracion',
-            'Mindfunless',
-            'Meditacion para dormir mejor',
+            "Meditaciones Tradicionales",
+            "Meditacion con visualizaciones",
+            "Relajacion y escaneo corporal",
+            "Tonos Binaturales",
+            "Afirmaciones",
+            "Meditacion guiada de atencion a la respiracion",
+            "Mindfunless",
+            "Meditacion para dormir mejor",
             // Asegúrate de tener los mismos nombres que en tu lista y en los casos del switch
           ].map((text, index) => (
-            <ListItem key={text} disablePadding onClick={() => handleListItemClick(text)}>
-              <ListItemButton>
+            <ListItem
+              key={text}
+              disablePadding
+              onClick={() => handleListItemClick(text)}
+            >
+              <ListItemButton
+                sx={{
+                  "&:hover": {
+                    bgcolor: "#F7E7DC", // Cambia el color de fondo al hacer hover
+                  },
+                }}
+              >
                 <ListItemText primary={text} />
               </ListItemButton>
             </ListItem>
           ))}
         </List>
       </Drawer>
-      <Box component="main" sx={{ flexGrow: 5, bgcolor: 'background.default', p: 3, marginLeft: '280px' }}>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 5,
+          bgcolor: "background.default",
+          p: 3,
+          marginLeft: "280px",
+        }}
+      >
         {/* Mostrar el texto seleccionado */}
         {renderSelectedContent()}
       </Box>
