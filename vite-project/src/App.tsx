8c844b0a068ec23ad4ec15788f4b478ddcf1457a
michael <garcia.michael.mrm@gmail.com>
2024-07-17 13:33:59 +0200

    registro + activacion de cuenta hecho

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -6,13 +6,14 @@ import CardRegistroP from './components/CardRegistro/CardRegistroP';
 import MeditacionGuiada from './components/MeditacionGuiada/MeditacionGuida';
 import ControlDiario from './components/ControlDiario/ControlDiario';
 import HomePage from './components/HomePage/HomePage';
+import ActivarCuenta from './components/CardRegistro/ActivarCuenta';
 function App() {
   return <>
   <NavBarComponent />
     <Routes>
     <Route path="/" element={<HomePage />} />
 
-      
+     <Route path="/activar/:token" element={<ActivarCuenta />} />
       <Route path="/login" element={<CardLogIn />} />
       <Route path="/login/Registrate" element={<CardRegistroP />}/>
       <Route path="/meditacion" element={<MeditacionGuiada />}/>
