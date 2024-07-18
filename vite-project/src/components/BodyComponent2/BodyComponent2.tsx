741bcd3da1b719f774beaaaa6b5e1fecc174cbe9
GPA9 <guillempereira@hotmail.com>
2024-07-18 12:54:29 +0200

    preguntas2

--- vite-project/src/components/BodyComponent2/BodyComponent2.tsx
+++ vite-project/src/components/BodyComponent2/BodyComponent2.tsx
@@ -10,19 +10,32 @@ import { styled } from '@mui/system';
 
 interface FAQItem {
   question: string;
-  answer: string;
+  answer: React.ReactNode;
 }
 
 const faqItems: FAQItem[] = [
-  { question: '¿Cuánto tiempo deberías dedicarle día a día a practicarlo?', answer: 'Los expertos recomiendan que, como mínimo entre 30 y 45 minutos al día son los necesarios para notar a largo plazo los efectos. Si puedes más, bienvenidos sean.' },
-  { question: '¿Cómo cancelo mi suscripción?', answer: 'Puedes cancelar tu suscripción en cualquier momento...' },
-  { question: '¿Puedo ver Netflix en mi dispositivo móvil?', answer: 'Sí, puedes ver Netflix en tu dispositivo móvil...' },
-  { question: '¿Cómo puedo contactar con el soporte de Netflix?', answer: 'Puedes contactar con nuestro soporte...' },
-  { question: '¿Cómo puedo encontrar películas en Netflix?', answer: 'Puedes buscar películas usando la función de búsqueda...' },
-  { question: '¿Qué es Netflix?', answer: 'Netflix es un servicio de streaming que ofrece una amplia variedad...' },
-  { question: '¿Puedo descargar contenido para verlo sin conexión?', answer: 'Sí, puedes descargar películas y series para verlas sin conexión...' },
-  { question: '¿Cómo puedo actualizar la información de mi cuenta?', answer: 'Puedes actualizar la información de tu cuenta accediendo a...' },
-  { question: '¿Cuántos dispositivos puedo usar con una cuenta de Netflix?', answer: 'Puedes usar hasta cuatro dispositivos con una cuenta de Netflix...' },
+  { question: '¿Cuánto tiempo deberías dedicarle día a día a practicarlo?', answer: '• Los expertos recomiendan que, como mínimo entre 30 y 45 minutos al día son los necesarios para notar a largo plazo los efectos. Si puedes más, bienvenidos sean.' },
+  { 
+    question: '¿Qué necesito para practicar meditación?', 
+    answer: (
+      <>
+        La meditación no es una práctica que requiera de numerosos recursos, los esenciales que necesitamos para poder practicar son:
+        <ul>
+          <li> • Un espacio tranquilo donde poder practicar</li>
+          <li> • Una esterilla</li>
+          <li> • Ropa cómoda</li>
+          <li> • Una almohada de meditación</li>
+        </ul>
+      </>
+    )
+  },
+  { question: '¿Qué debo hacer cuando los pensamientos vienen?', answer: 'Pensar es una función natural de la mente, no hay necesidad de detener los pensamientos que se produzcan. Cuando estás meditando y te das cuenta que has estado perdido en tus pensamientos, simplemente regresa tu conciencia al objeto de tu meditación. No hay que hacer nada más. Incluso puedes tomar tus pensamientos como el objeto de la meditación, o descansar en conciencia abierta, dejando que éstos vengan y vayan sin prestarles demasiada atención.' },
+  { question: '¿Cómo puedo contactar con el soporte de Netflix?', answer: '• Puedes contactar con nuestro soporte...' },
+  { question: '¿Cómo puedo encontrar películas en Netflix?', answer: '• Puedes buscar películas usando la función de búsqueda...' },
+  { question: '¿Qué es Netflix?', answer: '• Netflix es un servicio de streaming que ofrece una amplia variedad...' },
+  { question: '¿Puedo descargar contenido para verlo sin conexión?', answer: '• Sí, puedes descargar películas y series para verlas sin conexión...' },
+  { question: '¿Cómo puedo actualizar la información de mi cuenta?', answer: '• Puedes actualizar la información de tu cuenta accediendo a...' },
+  { question: '¿Cuántos dispositivos puedo usar con una cuenta de Netflix?', answer: '• Puedes usar hasta cuatro dispositivos con una cuenta de Netflix...' },
 ];
 
 const CustomAccordion = styled(Accordion)({
@@ -34,7 +47,7 @@ const CustomAccordion = styled(Accordion)({
     display: 'none', // Ocultar línea superior
   },
   '&:hover': {
-    backgroundColor: 'white', // Marrón en hover
+    backgroundColor: '#DEAC80', // Marrón en hover
     color: 'black',
   },
   '&.Mui-expanded': {
@@ -58,7 +71,7 @@ const BodyComponent2: React.FC = () => {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Box sx={{ p: 3, backgroundColor: '#AF8F6F', textAlign: 'center', minHeight: '100vh', fontFamily: 'monospace' }}>
+      <Box sx={{ p: 3, backgroundColor: 'white', textAlign: 'center', minHeight: '100vh', fontFamily: 'monospace' }}>
         <Typography variant="h4" gutterBottom sx={{ mb: 4, color: 'black', fontFamily: 'monospace' }}>
           Preguntas Frecuentes
         </Typography>
@@ -73,7 +86,9 @@ const BodyComponent2: React.FC = () => {
                 <Typography variant="subtitle1" sx={{ fontFamily: 'monospace', textAlign: 'left' }}>{item.question}</Typography>
               </CustomAccordionSummary>
               <AccordionDetails>
-                <Typography variant="body1" sx={{ fontFamily: 'monospace', textAlign: 'left' }}>{item.answer}</Typography>
+                <Typography variant="body1" sx={{ fontFamily: 'monospace', textAlign: 'left' }}>
+                  {item.answer}
+                </Typography>
               </AccordionDetails>
             </CustomAccordion>
           </Box>
