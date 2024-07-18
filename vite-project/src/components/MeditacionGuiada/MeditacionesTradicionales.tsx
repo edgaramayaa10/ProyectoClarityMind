70b135b885ceaf2b7cd37144f78fd947af70f21a
Edgar Amaya <160590740+edgaramayaa10@users.noreply.github.com>
2024-07-18 10:01:13 +0200

    cambio carpetas

--- vite-project/src/components/MeditacionGuiada/MeditacionesTradicionales.tsx
+++ vite-project/src/components/MeditacionGuiada/MeditacionesTradicionales.tsx
@@ -2,6 +2,9 @@
 import { Box } from '@mui/material';
 import Typography from '@mui/material/Typography';
 
+
+
+
 const MeditacionesTradicionales = () => {
   return (
     <Box sx={{textAlign: 'center'}}>
@@ -10,6 +13,8 @@ const MeditacionesTradicionales = () => {
       Se dan las instrucciones de forma oral, guiando a quien está escuchando para introducirlo en  un estado meditativo. <br />Suelen darse muchas pausas de sielncio, y no es frecuente acompañarlas con música. <br /> La gente suele recurrir a este tipo de meditación para iniciar o para mantener el estado de calma.
       </Typography>
     </Box>
+
+    
   );
 };
 
--- vite-project/src/components/MeditacionGuiada/TonosBinaurales.tsx
+++ vite-project/src/components/MeditacionGuiada/TonosBinaurales.tsx
@@ -5,7 +5,7 @@ import Typography from '@mui/material/Typography';
 const TonosBinaurales = () => {
   return (
     <Box>
-      <Typography variant="h5">Tonos Binaturales</Typography>
+      <Typography variant="h5">Tonos Binaurales</Typography>
       <Typography paragraph>
         Al presentarse dos sonidos con frecuencias diferentes en cada oreja, la mente trata de conciliar la diferencia creando una tercera onda. Se colocan unos cascos y se presenta un audio en el que en cada lado se presenta un sonido diferente.
       </Typography>
