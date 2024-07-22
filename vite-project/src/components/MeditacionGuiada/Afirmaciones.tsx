2cbea649dfdf0c4c16011b14afcd304ec525fd91
Edgar Amaya <160590740+edgaramayaa10@users.noreply.github.com>
2024-07-22 10:54:19 +0200

    audios

--- vite-project/src/components/MeditacionGuiada/Afirmaciones.tsx
+++ vite-project/src/components/MeditacionGuiada/Afirmaciones.tsx
@@ -1,16 +1,75 @@
-// MeditacionesTradicionales.tsx
-import { Box } from '@mui/material';
-import Typography from '@mui/material/Typography';
+import { Box, Typography, Grid } from '@mui/material';
+import muestra from '../IMG/5.png';
+import ReactPlayer from 'react-player';
+import Autoestima from '../Audio/Inicia tu día con AFIRMACIONES para Autoestima y Seguridad en ti 💛 - 5 minutos.mp3'
+import ReducionEstres from '../Audio/Meditación Guiada Afirmaciones para Reducir Estrés y Encontrar Paz 🧘_♀️✨.mp3'
+import Dormir from '../Audio/💤 Haz Esto 5 Minutos Antes de Dormir _ Afirmaciones Positivas _ Meditación de Wayne Dyer.mp3'
+import Chakras from '../Audio/❤️Meditación CORTA para ACTIVAR los 7 CHAKRAS ✨ con Afirmaciones Poderosas_ Meditacion Express.mp3'
+
+const tarjetas = [
+  {
+    titulo: 'Meditación de afirmaciones para buena autoestima' ,
+    audio: Autoestima,
+    imagen: muestra // URL de ejemplo
+  },
+  {
+    titulo: 'Meditación afirmaciones para reducir estres',
+    audio: ReducionEstres,
+    imagen: 'https://via.placeholder.com/150x300' // URL de ejemplo
+  },
+  {
+    titulo: 'Meditación afirmaciones antes de dormir',
+    audio: Dormir, 
+  },
+  {
+    titulo: 'Meditación activa los 7 chakras ',
+    audio: Chakras,
+    imagen: 'https://via.placeholder.com/150x300' // URL de ejemplo
+  },
+];
 
 const Afirmaciones = () => {
   return (
-    <Box sx={{marginTop:'100px'}}>
-      <Typography variant="h5">Afirmaciones</Typography>
-      <Typography paragraph>
-        En vez de pensar en negativo, este tipo de meditación ayuda a reformular estos pensamientos en un formato más optimista.
-      </Typography>
+    <Box sx={{ flexGrow: 1, padding: 2, marginTop: '100px' }}>
+      <Grid container spacing={3} direction="column">
+        {tarjetas.map((tarjeta, index) => (
+          <Grid item key={index}>
+            <Box sx={{ display: 'flex', alignItems: 'center', border: '1px solid #ddd', borderRadius: '8px', overflow: 'hidden' }}>
+              {/* Imagen a la izquierda */}
+              <Box 
+                sx={{ 
+                  width: 150, // Ancho de la imagen
+                  height: 150, // Altura fija
+                  backgroundImage: `url(${tarjeta.imagen})`,
+                  backgroundSize: 'cover', // Asegura que la imagen cubra el área
+                  backgroundPosition: 'center',
+                }} 
+              />
+              {/* Contenido de la tarjeta */}
+              <Box sx={{ flex: 1, padding: 2 }}>
+                <Typography variant="h5" component="div">
+                  {tarjeta.titulo}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  
+                </Typography>
+                {ReactPlayer.canPlay(tarjeta.audio) ? (
+                  <ReactPlayer url={tarjeta.audio} controls width="100%" height="50px" />
+                ) : (
+                  <audio controls>
+                    <source src={tarjeta.audio} type="audio/mpeg" />
+                    Tu navegador no soporta el elemento de audio.
+                  </audio>
+                )}
+              </Box>
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
     </Box>
   );
 };
 
 export default Afirmaciones;
+
+
--- vite-project/src/components/MeditacionGuiada/MeditacionRespiracion.tsx
+++ vite-project/src/components/MeditacionGuiada/MeditacionRespiracion.tsx
@@ -1,17 +1,75 @@
-// MeditacionesTradicionales.tsx
-import { AddBox } from '@mui/icons-material';
-import { Box } from '@mui/material';
-import Typography from '@mui/material/Typography';
+import { Box, Typography, Grid } from '@mui/material';
+import muestra from '../IMG/5.png';
+import ReactPlayer from 'react-player';
+import Respiracion from '../Audio/Mindfulness meditación guiada corta de 3 minutos - Atención a la respiración.mp3'
+import Profundo from '../Audio/Meditación Guiada_ Técnica de Respiración Profunda.mp3'
+import Consciente from '../Audio/Sencillo ejercicio de RESPIRACIÓN CONSCIENTE. (5 minutos).mp3'
+import calmar from '../Audio/Meditación para calmar tu mente. Atención a la respiración en 7 minutos (1).mp3'
+const tarjetas = [
+  {
+    titulo: 'Meditación atencion a la respiracion' ,
+    audio: Respiracion,
+    imagen: muestra // URL de ejemplo
+  },
+  {
+    titulo: 'Meditación respiracion profunda',
+    audio: Profundo,
+    imagen: 'https://via.placeholder.com/150x300' // URL de ejemplo
+  },
+  {
+    titulo: 'Meditación respiracion consciente',
+    audio: Consciente, 
+  },
+  {
+    titulo: 'Meditación respiracion para calmar tu mente ',
+    audio: calmar,
+    imagen: 'https://via.placeholder.com/150x300' // URL de ejemplo
+  },
+];
 
 const MeditacionRespiracion = () => {
   return (
-    <Box sx={{marginTop:'100px'}}>
-      <Typography variant="h5">Meditacion guiada de atencion a la respiracion</Typography>
-      <Typography paragraph>
-        La premisa de este tipo de meditación es que si se logra controlar algo tan simple como la respiración, se puede entrenar la mente en casi cualquier aspecto.
-      </Typography>
+    <Box sx={{ flexGrow: 1, padding: 2, marginTop: '100px' }}>
+      <Grid container spacing={3} direction="column">
+        {tarjetas.map((tarjeta, index) => (
+          <Grid item key={index}>
+            <Box sx={{ display: 'flex', alignItems: 'center', border: '1px solid #ddd', borderRadius: '8px', overflow: 'hidden' }}>
+              {/* Imagen a la izquierda */}
+              <Box 
+                sx={{ 
+                  width: 150, // Ancho de la imagen
+                  height: 150, // Altura fija
+                  backgroundImage: `url(${tarjeta.imagen})`,
+                  backgroundSize: 'cover', // Asegura que la imagen cubra el área
+                  backgroundPosition: 'center',
+                }} 
+              />
+              {/* Contenido de la tarjeta */}
+              <Box sx={{ flex: 1, padding: 2 }}>
+                <Typography variant="h5" component="div">
+                  {tarjeta.titulo}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  
+                </Typography>
+                {ReactPlayer.canPlay(tarjeta.audio) ? (
+                  <ReactPlayer url={tarjeta.audio} controls width="100%" height="50px" />
+                ) : (
+                  <audio controls>
+                    <source src={tarjeta.audio} type="audio/mpeg" />
+                    Tu navegador no soporta el elemento de audio.
+                  </audio>
+                )}
+              </Box>
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
     </Box>
   );
 };
 
 export default MeditacionRespiracion;
+
+
+
