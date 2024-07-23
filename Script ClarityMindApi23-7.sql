f9bc7075ed3c503a257d2bf56e6ac945b7dda6b8
michael garcia <garcia.michael.mrm@gmail.com>
2024-07-23 12:08:39 +0200

    Add files via upload

---
+++ Script ClarityMindApi23-7.sql
@@ -0,0 +1,8 @@
+DROP DATABASE IF EXISTS DB_CLARITY_MIND;
+CREATE SCHEMA IF NOT EXISTS DB_CLARITY_MIND;
+USE DB_CLARITY_MIND;
+SELECT * FROM usuarios;
+DELETE FROM usuarios;
+ALTER TABLE usuarios ADD COLUMN token VARCHAR(255); #esto es por si no se crea el campo al arrancar la api
+ALTER TABLE usuarios CHANGE COLUMN contraseña contrasenya VARCHAR(255);
+ALTER TABLE usuarios ADD COLUMN sesion_activa BOOLEAN DEFAULT FALSE;
